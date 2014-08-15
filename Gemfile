aa7adcbc6fe4adfdbacc0ce5c63f2b7f04d92d1e
eganpg <eganpg@gmail.com>
2014-08-15 14:25:54 -0700

    initial validators

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'https://rubygems.org'
 
 gem 'nokogiri'
 
-gem 'open-uri'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.1.4'
 # Use SCSS for stylesheets
--- app/models/login.rb
+++ app/models/login.rb
@@ -8,7 +8,15 @@ class Login
   field :username, type: String
   field :password_digest, type: String
 
+  
+  
+  validates :username, presence: true
+  validates :password_digest, presence: true 
+  validates_length_of :password_digest, minimum: 8, maximum: 16
+  validates_uniqueness_of :username
   has_secure_password
+
+
 
   has_many :mainorderboards
   has_many :suborders
